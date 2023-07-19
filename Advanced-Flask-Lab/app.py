77a5689a050b97f57607802f3803d980bc01527f
Samarsh24-meet <139688951+Samarsh24-meet@users.noreply.github.com>
2023-07-19 10:33:35 +0300

    Done :)

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -9,23 +9,22 @@ app = Flask(  # Create a flask app
 )
 
 # Variables for tasks
-image_link = "https://uploads-ssl.webflow.com/5dd64bd3a930f9d04abd1363/5de254f85f1762feee30d664_meet_logo_red.png"
+image_link = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1G50YJw6brINH7RzO60IlGpfJOSWOBJBcJA&usqp=CAU"
 
-user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders."
+user_bio = "Woooo Wooooooo a very cool description warning"
 
 posts = {
-    "https://imageio.forbes.com/blogs-images/samarmarwan/files/2018/03/MEET-Students-1200x800.jpg": "Group projects <3",
-    "https://uploads-ssl.webflow.com/5dd64bd3a930f96c82bd137a/63024ce64d943673cb004a4c_2022.07.17%20-%20Summer%20Day%201.png": "MEET summer!",
-    "https://uploads-ssl.webflow.com/5dd64bd3a930f9d04abd1363/5de6d502d6d70c0ad49a060c_6.jpg": "#MEET_DU",
+    "https://www.tfront.com/images/product/icon/2605.jpg": "cool",
+    "https://www.musikalessons.com/blog/wp-content/uploads/2017/01/jazz-piano.jpg": "very coollll!",
+    "https://uploads-ssl.webflow.com/5dd64bd3a930f9d04abd1363/5de6d502d6d70c0ad49a060c_6.jpg": "Meet content cuz why not!",
     "https://global-uploads.webflow.com/5fe28feebcae602620061802/5fe5401840671a36cd1d47d5_5de6d5024dd1a74670173aed_1-p-1080.jpeg": "Our lovely TAs!"}
-
 
 #####
 
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html',i = image_link, u = user_bio, p = posts)
 
 
 @app.route('/about')  # '/' for the default page
